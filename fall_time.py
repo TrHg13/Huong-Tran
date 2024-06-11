7698d2ede94631693b6005b0c30ca09a88cd3af8
TrHg13 <asiimovtashigi@gmail.com>
2024-06-10 22:18:07 -0700

    WEEK 7

--- fall_time.py
+++ fall_time.py
@@ -1,11 +1,13 @@
+# measurements.py
+
 # Import the required modules
 import math
-import constants
+from constants import earth_g, mars_g
 
-# Get the height from the user
-height = int(input('Height in meters: '))  # Meters from planet
+# Input: Height in meters
+height = int(input('Height in meters: '))
 
-# Calculate and print the time values
+# Main code to calculate and print the time to fall from the given height on Earth and Mars
 if __name__ == '__main__':
-    print('Earth:', math.sqrt(2 * height / constants.earth_g), 'seconds')
-    print('Mars:', math.sqrt(2 * height / constants.mars_g), 'seconds')
+    print('Earth:', math.sqrt(2 * height / earth_g), 'seconds')
+    print('Mars:', math.sqrt(2 * height / mars_g), 'seconds')
